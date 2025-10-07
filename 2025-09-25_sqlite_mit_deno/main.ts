bf5f48e26742dd0f09e554a2fb13016404b4ef64
Mankah Beyer-Ngwa <BEY240097@spengergasse.at>
2025-10-07 14:15:10 +0200

    hü

--- 2025-09-25_sqlite_mit_deno/main.ts
+++ 2025-09-25_sqlite_mit_deno/main.ts
@@ -1,6 +1,11 @@
 import { DatabaseSync } from "node:sqlite";
 
 const db = new DatabaseSync("2ahwii.db");
-const stmt = db.prepare("SELECT * FROM students");
+let stmt = db.prepare("SELECT * FROM students");
 const rows = stmt.all();
 console.log(rows);
+
+stmt=db.prepare("INSERT INTO students (id,name,birthdate) VALUES (?, ?, ?)");
+stmt.run(100,"Jana","1098-21-11");
+
+console.log(stmt.all());
